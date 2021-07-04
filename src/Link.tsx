2f66179da50e9ef8dd3b0c0cd15efa2ef5ca9d36
Mikkel-T <mikkel@mikkel-t.com>
2021-07-04 22:13:48 +0200

    Update Link.tsx

--- src/Link.tsx
+++ src/Link.tsx
@@ -4,7 +4,7 @@ export default function Link({ text, href }: { text: string; href: string }) {
   return (
     <a
       href={href}
-      className="hover:text-blue-800 text-blue-600"
+      className="hover:underline text-blue-600"
       target="_blank"
       rel="noopener noreferrer"
     >
