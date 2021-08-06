4e1eb30ca3cd769422e02956c7263903a0fb5fc4
Mikkel-T <mikkel@mikkel-t.com>
2021-08-06 08:13:03 +0200

    Reset settings button and more styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
+import {
+  ChevronDownIcon,
+  ChevronRightIcon,
+  RefreshIcon,
+} from '@heroicons/react/solid';
 import React, { useEffect, useState } from 'react';
-import { ChevronDownIcon, ChevronRightIcon } from '@heroicons/react/solid';
-import CharsToCount from './settings/CharsToCount';
+
 import Link from './Link';
+import CharsToCount from './settings/CharsToCount';
 
 function App() {
   const [text, setText] = useState<string>('');
   const [count, setCount] = useState<number>(0);
+
+  const [settingsOpen, setSettingsOpen] = useState<boolean>(false);
   const [charsPerPage, setCharsPerPage] = useState<number | string>(1300);
   const [charsToCount, setCharsToCount] = useState<Record<string, boolean>>({
     bogstaver: true,
@@ -13,7 +20,7 @@ function App() {
     mellemrum: false,
     andre: false,
   });
-  const [settingsOpen, setSettingsOpen] = useState<boolean>(false);
+
   useEffect(() => {
     const chars = localStorage.getItem('charsPerPage');
     if (chars) setCharsPerPage(+chars);
@@ -22,13 +29,12 @@ function App() {
   }, []);
 
   useEffect(() => {
-    let num = 0;
-    if (charsToCount.bogstaver) num += text.match(/[A-ZÀ-ÚÄ-Ü]/gi)?.length || 0;
-    if (charsToCount.tal) num += text.match(/[0-9]/gi)?.length || 0;
-    if (charsToCount.mellemrum) num += text.match(/[ ]/gi)?.length || 0;
-    if (charsToCount.andre)
-      num += text.match(/[^A-ZÀ-ÚÄ-Ü0-9 ]/gi)?.length || 0;
-    setCount(num);
+    let n = 0;
+    if (charsToCount.bogstaver) n += text.match(/[A-ZÀ-ÚÄ-Ü]/gi)?.length || 0;
+    if (charsToCount.tal) n += text.match(/[0-9]/gi)?.length || 0;
+    if (charsToCount.mellemrum) n += text.match(/[ ]/gi)?.length || 0;
+    if (charsToCount.andre) n += text.match(/[^A-ZÀ-ÚÄ-Ü0-9 ]/gi)?.length || 0;
+    setCount(n);
   }, [text, charsPerPage, charsToCount]);
 
   return (
@@ -63,38 +69,27 @@ function App() {
             )}
             Indstillinger
           </div>
+          <br />
           {settingsOpen && (
-            <div>
-              <div>
+            <div className="rounded-xl px-7 inline-block p-1 bg-gray-100">
+              <div className="my-2">
                 <p className="font-semibold">
                   Vælg hvilke slags tegn der skal tælles med:
                 </p>
-                <CharsToCount
-                  name="bogstaver"
-                  title="Bogstaver"
-                  onChange={setCharsToCount}
-                  state={charsToCount}
-                />
-                <CharsToCount
-                  name="tal"
-                  title="Tal"
-                  onChange={setCharsToCount}
-                  state={charsToCount}
-                />
-                <CharsToCount
-                  name="mellemrum"
-                  title="Mellemrum"
-                  onChange={setCharsToCount}
-                  state={charsToCount}
-                />
-                <CharsToCount
-                  name="andre"
-                  title="Alle andre tegn"
-                  onChange={setCharsToCount}
-                  state={charsToCount}
-                />
+                {Object.keys(charsToCount).map((i) => (
+                  <CharsToCount
+                    name={i}
+                    title={
+                      i !== 'andre'
+                        ? i.charAt(0).toUpperCase() + i.slice(1)
+                        : 'Alle andre tegn'
+                    }
+                    onChange={setCharsToCount}
+                    state={charsToCount}
+                  />
+                ))}
               </div>
-              <div>
+              <div className="my-2">
                 <p className="font-semibold">
                   Antal talte tegn per normalside:
                 </p>
@@ -108,6 +103,24 @@ function App() {
                   className="focus:border-gray-500 p-2 border rounded-md outline-none"
                 />
               </div>
+              <div className="my-2">
+                <button
+                  onClick={() => {
+                    setCharsToCount({
+                      bogstaver: true,
+                      tal: true,
+                      mellemrum: false,
+                      andre: false,
+                    });
+                    setCharsPerPage(1300);
+                    localStorage.clear();
+                  }}
+                  className="text-gray-50 focus:outline-none hover:bg-red-500 focus:bg-red-500 p-2 px-2 mt-3 bg-red-600 rounded-md"
+                >
+                  <RefreshIcon className="inline-block w-5 h-5 mr-1" />
+                  Nulstil indstillinger
+                </button>
+              </div>
             </div>
           )}
         </div>
@@ -116,7 +129,7 @@ function App() {
             onClick={() => setText('')}
             className="text-gray-50 focus:outline-none hover:bg-blue-600 focus:bg-blue-600 p-2 px-4 mt-3 bg-blue-700 rounded-md"
           >
-            Nulstil
+            Ryd tekstfelt
           </button>
         </div>
       </main>
@@ -131,7 +144,7 @@ function App() {
         </p>
         <p className="mb-4 text-sm text-gray-600">
           Programmet beregner antal normalsider ved at dividere antal tegn der
-          tælles med (Kan sættes i indstillinger) i teksten med {charsPerPage}.
+          tælles med i teksten (Kan sættes i indstillinger) med {charsPerPage}.
           <br />
           Hvis du finder en fejl, må du meget gerne rapportere den{' '}
           <Link
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'tailwindcss/tailwind.css';
+
 import App from './App';
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
